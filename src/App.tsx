e71bba39654f14625ac2fc3c284e0cecebf26d00
Victor Lucas A. de Oliveira <59880314+victorlucass@users.noreply.github.com>
2022-11-08 08:10:15 -0400

    feat: config theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
 function App() {
- return <></>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>hello world</h1>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
