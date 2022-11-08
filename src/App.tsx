71b0dacace6fa4f11afad86b3fd464b69165ae22
Victor Lucas A. de Oliveira <59880314+victorlucass@users.noreply.github.com>
2022-11-08 08:48:39 -0400

    feat: creating header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/transactions";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/theme/default";
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>hello world</h1>
+      <Transactions />
       <GlobalStyles />
     </ThemeProvider>
   );
---
+++ src/components/header/styled.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background: ${(props) => props.theme["gray-900"]};
+  padding: 2.5rem 0 7.625rem;
+`;
+export const HeaderContent = styled.div`
+  max-width: ${transformPxInRem(1200)};
+  margin: 0 auto;
+  padding: 0 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NewTransactionButton = styled.button`
+  padding: ${transformPxInRem(12)} ${transformPxInRem(20)};
+  border: 0;
+  border-radius: 6px;
+  background: ${(props) => props.theme["green-300"]};
+  color: ${(props) => props.theme["white"]};
+  font-weight: bold;
+  transition: background-color 0.5s;
+  cursor: pointer;
+  &:hover {
+    background: ${(props) => props.theme["green-700"]};
+  }
+`;
+
+function transformPxInRem(px: number): string {
+  return `${px / 16}rem`;
+}
